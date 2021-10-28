038c98171f04f6cd6e45a120d905a1a31bc9567e
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-10-28 19:10:23 +0530

    Update main.py
    Added REST API Creation using flask.

--- main.py
+++ main.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
+
+@app.route('/armstrong/<int:n>')
+def armstrong(n):
+    sum=0
+    order=len(str(n))
+    copy_n=n
+    while(n>0):
+        digit=n%10
+        sum+=digit **order
+        n=n//10
+
+    if(sum==copy_n):
+        print(f"{copy_n} is armstrong ")
+        return "True"
+    else:
+        print(f"{copy_n} is not armstrong ")
+        return "False"
 
 if __name__ == "__main__":
     app.run(debug=True)
